ac2d96e9c1eabfc25c59f04f6db7d4d8d426a367
gxizhe <gxizhe@gmail.com>
2018-10-04 20:20:15 -0400

    Shit

--- Class Notes/src/lec07/StringListInterface.java
+++
@@ -1,9 +0,0 @@
-package lec07;
-
-public interface StringListInterface {
-    public void add(String s);
-    public void add(int i, String s) throws IndexOutOfBoundsException;
-    public String remove(int i) throws IndexOutOfBoundsException;
-    public String get(int i) throws IndexOutOfBoundsException;
-    public int size();
-}
